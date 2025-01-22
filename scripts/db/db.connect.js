2279c2040ca14a0ce390447aba26fc3190eaf1b8
derrickdryer <contact@derrickdryer.xyz>
2025-01-22 15:51:04 -0500

    Fixed some issues

--- scripts/db/db.connect.js
+++ scripts/db/db.connect.js
@@ -12,7 +12,8 @@ async function connectToDB() {
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT
+        port: process.env.DB_PORT,
+        database: process.env.DB_DATABASE
     });
 
     console.log('Connected to Maria DB!');
