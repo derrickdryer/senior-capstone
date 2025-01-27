99f6dd749537e1cfe1a9dbd71c5a66250d1a969f
derrickdryer <contact@derrickdryer.xyz>
2025-01-27 15:05:31 -0500

    Remove unneeded files

--- database.js
+++ database.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 
 // Create a connection pool
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '4fGc*sDRYgpKRpDRH^^5',
-  database: process.env.DB_DATABASE || 'realtor_website',
-  port: process.env.DB_PORT || 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
