32697900390a8afd3a623954d848dc259edc985a
derrickdryer <contact@derrickdryer.xyz>
2025-02-10 15:11:38 -0500

    Remove .html from header.js

--- app/components/header.js
+++ app/components/header.js
@@ -6,11 +6,11 @@ export function createHeader() {
         <h1>Russell Properties LLC (Development Build)</h1>
         <nav>
             <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="properties.html">Properties</a></li>
-                <li><a href="about.html">About Us</a></li>
-                <li><a href="contact.html">Contact</a></li>
-                <li><a href="login.html">Login</a></li>
+                <li><a href="/">Home</a></li>
+                <li><a href="properties">Properties</a></li>
+                <li><a href="about">About Us</a></li>
+                <li><a href="contact">Contact</a></li>
+                <li><a href="login">Login</a></li>
             </ul>
         </nav>
     `;
