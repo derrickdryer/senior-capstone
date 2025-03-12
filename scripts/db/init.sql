c48460f8757f906f3adf38d89836eec6430fa10b
derrickdryer <contact@derrickdryer.xyz>
2025-03-11 23:36:23 -0400

    Fix init.sql

--- scripts/db/init.sql
+++ scripts/db/init.sql
@@ -84,7 +84,7 @@ CREATE TABLE IF NOT EXISTS users (
     role ENUM('manager', 'maintenance', 'tenant') NOT NULL,
     username VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
 );
 
 -- Table: notifications
